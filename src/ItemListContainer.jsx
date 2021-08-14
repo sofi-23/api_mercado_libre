c2d6958f7fc3379896d5838388616193b074d45e
Sofia Constenla <sofi.amc1995@gmail.com>
2021-08-14 19:28:41 -0300

    muestra 1 solo producto 3 veces

--- src/ItemListContainer.jsx
+++ src/ItemListContainer.jsx
@@ -1,8 +1,29 @@
+
+import { useEffect, useState } from 'react';
 import ItemDetail from './ItemDetail';
 
 export default function ItemListContainer () {
-
-    return (<>
-    <ItemDetail name="Pelota" description="Una pelota de futbol comun y corriente" price="600 USD" stock="10 u"/>
-    </>)
+    const [itemList, setItemList] = useState([])
+    const getItems = () => { 
+        return fetch('https://api.mercadolibre.com/sites/MLA/search?q=pelota')
+        .then(response => {
+            return response.json()
+        }).then(data => {
+            return data
+        } )
+    }
+    useEffect(async () => { //Acá el useEffect hace que se ejecute una sola vez.
+        const data = await getItems(new Set()); //new Set arma un array en el que no se pueden repetir elementos
+        data.results.forEach(item=> {
+            const arr = [...itemList]; 
+            arr.push(<ItemDetail key={item.id} name={item.title} description={item.id} price={item.price}/>);
+            setItemList(arr)
+        })
+    },[])
+    
+    return (
+    <>
+        {itemList}
+    </>
+    )
 }
